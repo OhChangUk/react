512e3a26f5b2983bfdb773283242717f5c0b9e42
오창욱 <o2556@naver.com>
2023-08-25 13:11:21 +0900

    변경2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          파일변경됨..
+          파일변경됨
         </p>
         <a
           className="App-link"
